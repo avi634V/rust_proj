99bae151cb5669ca15b10720d5ce8ff34304f066
avi634V <avi634V@gmail.com>
2025-03-05 20:20:09 +0530

    struct

--- project19/src/main.rs
+++ project19/src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+// struct Person {
+//     name: String,
+//     age: u8,
+// }
+
+// fn main(){
+
+// let mut user = Person {
+//     name: String::from("Alice"),
+//     age: 30,
+// };
+// user.age = 31;
+// println!("User new age is :{}",user.age);
+// }
+
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
 }
+ fn main(){
+     let user1 = build_user(String::from("      "),String::from("       "));
+        println!("User1 email is :{}",user1.email); 
+ }
+ fn build_user(email:String,username: String) -> User{
+        
+    User{
+        active:true,
+        username:String:: from ("Avijit"),
+        email:String:: from ("avijit@gmail.com"),
+        sign_in_count:1,
+    }
+ }
---
+++ target/debug/deps/project19-8175e44a1b743c00.d
@@ -0,0 +1,5 @@
+/Users/avijitpandey/Desktop/rust-project/rust_prog/target/debug/deps/libproject19-8175e44a1b743c00.rmeta: project19/src/main.rs
+
+/Users/avijitpandey/Desktop/rust-project/rust_prog/target/debug/deps/project19-8175e44a1b743c00.d: project19/src/main.rs
+
+project19/src/main.rs:
